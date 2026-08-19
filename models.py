ca8c0e81cb0cf33af5ddd1b9aa28541cc40444be
canusangel99@gmail.com <canusangel99@gmial.com>
2026-08-18 20:12:53 -0600

    Architecture sale: Docker + FastAPI + Alembic configuration

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -22,3 +22,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    # test for alembic fuction
+    is_active = Column(Boolean, default=True)
+    # other test
+    phone = Column(String, nullable=True)
